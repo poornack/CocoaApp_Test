7423d6548c14b185bc916df470d0a97ff510272d
Poorna Chander Kalidas <Poorna@Poornas-MacBook-Pro.local>
2017-07-23 02:04:14 -0700

    Initial Commit

---
+++ CocoaApp_Test/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CocoaApp_Test
+//
+//  Created by Poorna Chander Kalidas on 2017-07-23.
+//  Copyright © 2017 Poorna Chander Kalidas. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CocoaApp_Test/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CocoaApp_Test
+//
+//  Created by Poorna Chander Kalidas on 2017-07-23.
+//  Copyright © 2017 Poorna Chander Kalidas. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
